3ec022520d2d469bc207f5bf3fe04cc7fb1f107d
og2509 <og2509@naver.com>
2018-10-17 13:10:56 +0200

    ss

---
+++ src/pl/waw/sgh/DoubleFloat.java
@@ -0,0 +1,13 @@
+package pl.waw.sgh;
+
+public class DoubleFloat {
+
+    public static void main(String[] args) {
+        double d1 = 3425;
+        double d2 = 34235;
+        float f1 = d1
+        float f2 = d1
+
+        System.out.println(d1 * d2);
+    }
+}
